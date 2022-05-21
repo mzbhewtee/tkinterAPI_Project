3da7f2d00390b54d1e0e2257929277698777fb88
mzbhewtee <abimbolaikus@gmail.com>
2022-05-21 22:48:14 +0100

    Edited

--- api.py
+++ api.py
@@ -6,18 +6,14 @@ import requests
 from requests import get
 import datetime
 from dotenv import load_dotenv
+import pandas as pd
 
 load_dotenv() 
 
 api = os.environ.get('api')
 api2 = os.environ.get('api2')
 
-uri = 'https://api.football-data.org/v4/matches'
-headers = { 'X-Auth-Token': api2 }
 
-response = requests.get(uri, headers=headers)
-for match in response.json()['matches']:
-  print (match)
 """
 Retrive random quotes from api forismatic
 """
@@ -32,9 +28,9 @@ def get_quotes():
 """
 Retrieve the current weather forecast from OpenWeatherMap.
 """
-def get_weather_forecast(coords={'lat': -1.9441, 'lon': 30.0619}): # default location at Cape Canaveral, FL
+def get_weather_forecast(coords={'lat': -1.9441, 'lon': 30.0619}): # default location at Kigali,
     try: # retrieve forecast for specified coordinates
-        # api = '6dce9c12713225dcb83453c986263b9c' # replace with your own OpenWeatherMap API key
+        
         url = f'https://api.openweathermap.org/data/2.5/forecast?lat={coords["lat"]}&lon={coords["lon"]}&appid={api}&units=metric'
         data = json.load(request.urlopen(url))
 
@@ -45,32 +41,33 @@ def get_weather_forecast(coords={'lat': -1.9441, 'lon': 30.0619}): # default loc
         for period in data['list'][0:9]: # populate list with next 9 forecast periods 
             forecast['periods'].append({'timestamp': datetime.datetime.fromtimestamp(period['dt']),
                                         'temp': round(period['main']['temp']),
-                                        'description': period['weather'][0]['description'].title(),
-                                        'icon': f'http://openweathermap.org/img/wn/{period["weather"][0]["icon"]}.png'})
+                                        'description': period['weather'][0]['description'].title()})
+                                        # 'icon': f'http://openweathermap.org/img/wn/{period["weather"][0]["icon"]}.png'})
         
         return forecast
 
     except Exception as e:
         print("\nThere are issues when generating today's weather forecast",e)        
 
-def get_twitter_trends():
-    pass
 
-def get_wikipedia_article():
-    pass
+def matches():
+    try:
+        uri = 'https://api.football-data.org/v4/matches'
+        headers = { 'X-Auth-Token': api2 }
+        response = (requests.get(uri, headers=headers)).json()
+
+        for match in response['matches']:
+            df = pd.DataFrame(match)
+            print(df)
+
+    except Exception as e:
+        print(" Sorry, We couldn't get today's match", e)
+
 
 if __name__ == '__main__':
-    ##### test get_random_quote() #####
-    # print('\nTesting quote generation...')
+   
     print("------ Quote of the day ------")
     print(get_quotes())
-    # print(f' - Random quote is "{quote["quote"]}" - {quote["author"]}')
-
-    # quote = get_random_quote(quotes_file = None)
-    # print(f' - Default quote is "{quote["quote"]}" - {quote["author"]}')
-
-    ##### test get_weather_forecast() #####
-    # print('\nTesting weather forecast retrieval...')
 
     forecast = get_weather_forecast() # get forecast for default location
     if forecast:
@@ -78,14 +75,5 @@ if __name__ == '__main__':
         for period in forecast['periods']:
             print(f' - {period["timestamp"]} | {period["temp"]}°C | {period["description"]}')
 
-    # austin = {'lat': 30.2748,'lon': -97.7404} # coordinates for Texas State Capitol
-    # forecast = get_weather_forecast(coords = austin) # get Austin, TX forecast
-    # if forecast:
-    #     print(f'\nWeather forecast for {forecast["city"]}, {forecast["country"]} is...')
-    #     for period in forecast['periods']:
-    #         print(f' - {period["timestamp"]} | {period["temp"]}°C | {period["description"]}')
-
-    # invalid = {'lat': 1234.5678 ,'lon': 1234.5678} # invalid coordinates
-    # forecast = get_weather_forecast(coords = invalid) # get forecast for invalid location
-    # if forecast is None:
-    #     print('Weather forecast for invalid coordinates returned None')
+    print("\n---Football matches---")
+    print(matches())
